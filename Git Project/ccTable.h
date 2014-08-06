9155981b973b586ed51f3458bca814609891cb33
Saba mohebpour <sabamm@Sabas-MacBook-Air.local>
2014-08-05 23:14:33 -0700

    added a test class

--- Git Project/ccTable.h
+++ Git Project/ccTable.h
@@ -1,13 +1,13 @@
 //
-//  ccViewController.h
+//  ccTable.h
 //  Git Project
 //
 //  Created by Saba mohebpour on 2014-08-05.
 //  Copyright (c) 2014 ___saba Company___. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
-@interface ccViewController : UIViewController
+@interface ccTable : NSObject
 
 @end
---
+++ Git Project/ccTable.m
@@ -0,0 +1,13 @@
+//
+//  ccTable.m
+//  Git Project
+//
+//  Created by Saba mohebpour on 2014-08-05.
+//  Copyright (c) 2014 ___saba Company___. All rights reserved.
+//
+
+#import "ccTable.h"
+
+@implementation ccTable
+
+@end
